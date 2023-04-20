f540de361b7429013d6d638ad56b271c612910c3
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2023-04-20 15:03:43 +0100

    Added custom 404 error page to match styling of website

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,10 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -41,8 +43,8 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        // $this->reportable(function (Throwable $e) {
+        //     //
+        // });
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="flex flex-col items-center justify-center mt-48 space-y-5">
+        <h1 class="font-extrabold text-primary">Error!</h1>
+        <p class="text-2xl">Sorry! Page cannot be found!</p>
+        <a class="px-5 py-2 text-white shadow-lg rounded-xl border-primary bg-primary hover:bg-background"
+            href="/">Go Back to Homepage</a>
+    </div>
+
+</x-layout>
