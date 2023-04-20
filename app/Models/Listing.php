527db4744e67ad62cc3177e21d320711a0fbc5fb
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2023-04-20 15:24:19 +0100

    Added search function for search bar and clicking on skill tags. Added pagniation for listing results and changed default loading or listings to be latest first

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -10,4 +10,18 @@ class Listing extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'user_id', 'company', 'location', 'website', 'email', 'description', 'tags', 'logo'];
+
+    // Search Function
+    public function scopeFilter($query, array $filters)
+    {
+        // Filter by Tag
+        if ($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+
+        // Filter by search term
+        if ($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')->orWhere('description', 'like', '%' . request('search') . '%')->orWhere('tags', 'like', '%' . request('search') . '%')->orWhere('location', 'like', '%' . request('search') . '%');
+        }
+    }
 }
--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -2,7 +2,7 @@
     @include('partials._hero')
     @include('partials._search')
 
-    <div class="mx-4 gap-4 space-y-4 pt-1 md:space-y-0 lg:grid lg:grid-cols-3">
+    <div class="gap-4 pt-1 mx-4 space-y-4 md:space-y-0 lg:grid lg:grid-cols-3">
         @unless (count($listings) == 0)
             @foreach ($listings as $listing)
                 <x-listing-card :listing="$listing" />
@@ -10,6 +10,10 @@
         @else
             <p>No Listings Found</p>
         @endunless
+    </div>
+
+    <div class="p-4 mt-6">
+        {{ $listings->links() }}
     </div>
 
 </x-layout>
